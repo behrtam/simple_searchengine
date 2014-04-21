73911e350d1b889f95e31ba7c46ce45f11ec372d
Florian <florian.willich@students.htw-berlin.de>
2014-04-21 19:13:35 +0200

    downloader func gebaut und getestet

--- downloader.py
+++ downloader.py
@@ -1,2 +1,22 @@
 #!/usr/local/sbin/python
-print("Hello World!")
+import urllib2
+
+def downloader(urls):
+	html = []
+
+	for x in urls:
+		source = urllib2.urlopen(x)
+		html.append( source.read() ) 
+		source.close()
+
+	return html
+
+url1 = "http://mysql12.f4.htw-berlin.de/crawl/d01.html"
+url2 = "http://mysql12.f4.htw-berlin.de/crawl/d06.html"
+url3 = "http://mysql12.f4.htw-berlin.de/crawl/d08.html"
+urls = [url1, url2, url3]
+
+pages = downloader(urls)
+
+for k in pages:
+		print k
